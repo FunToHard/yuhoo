469c9e9a23d7ea1c4af5e97be9c5ccf77c5897fc
FunToHard <legendbibek998@gmail.com>
2026-01-29 21:06:39 +0530

    feat: Sidebar, SearchBar, Account Button & pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,44 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/watch_list_page.dart';
+import 'pages/coming_soon_page.dart';
+import 'pages/discovery_page.dart';
+import 'pages/friends_page.dart';
+import 'pages/parties_page.dart';
+import 'pages/media_page.dart';
+import 'pages/settings_page.dart';
+import 'widgets/sidebar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+enum NavPage {
+  home,
+  watchList,
+  comingSoon,
+  discovery,
+  friends,
+  parties,
+  media,
+  settings,
+  logout,
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'YuHoo',
+      theme: ThemeData(primarySwatch: Colors.red, useMaterial3: true),
+      home: const MyHomePage(title: 'YuHoo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +47,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  NavPage _currentPage = NavPage.home;
 
-  void _incrementCounter() {
+  void _navigateTo(NavPage page) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentPage = page;
     });
+    Navigator.pop(context); // Close drawer after navigation
+  }
+
+  Widget _buildPage() {
+    switch (_currentPage) {
+      case NavPage.home:
+        return const HomePage();
+      case NavPage.watchList:
+        return const WatchListPage();
+      case NavPage.comingSoon:
+        return const ComingSoonPage();
+      case NavPage.discovery:
+        return const DiscoveryPage();
+      case NavPage.friends:
+        return const FriendsPage();
+      case NavPage.parties:
+        return const PartiesPage();
+      case NavPage.media:
+        return const MediaPage();
+      case NavPage.settings:
+        return const SettingsPage();
+      case NavPage.logout:
+        return const HomePage(); // Placeholder
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        centerTitle: false,
+        actions: [
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                height: 40,
+                width: 300,
+                child: SearchBar(
+                  hintText: 'Search here...',
+                  leading: const Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Icon(Icons.search),
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle, size: 32),
+            onPressed: () {
+              // Handle account icon tap
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      drawer: Sidebar(onNavigate: _navigateTo, currentPage: _currentPage),
+      body: _buildPage(),
     );
   }
 }
